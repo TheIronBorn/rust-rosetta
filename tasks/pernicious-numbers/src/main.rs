27734c2e9452f7af62df3c1dd6742fd29607b96b
TheIronBorn <danielwatson311@gmail.com>
2018-01-17 23:52:05 -0800

    pernicious

--- tasks/pernicious-numbers/src/main.rs
+++ tasks/pernicious-numbers/src/main.rs
@@ -5,22 +5,22 @@ use std::ops::RangeFrom;
 
 use aks_test_for_primes::is_prime;
 
+fn pernicious() -> Filter<RangeFrom<usize>, fn(&usize) -> bool> {
+    (0..).filter(is_pernicious as fn(&usize) -> bool)
+}
+
+fn is_pernicious(n: &usize) -> bool {
+    is_prime(n.count_ones())
+}
+
 fn main() {
     for i in pernicious().take(25) {
         print!("{} ", i);
     }
-    println!("");
-    for i in (888_888_877u64..888_888_888).filter(is_pernicious) {
+    println!();
+    for i in (888_888_877..888_888_888).filter(is_pernicious) {
         print!("{} ", i);
     }
-}
-
-fn pernicious() -> Filter<RangeFrom<u64>, fn(&u64) -> bool> {
-    (0u64..).filter(is_pernicious as fn(&u64) -> bool)
-}
-
-fn is_pernicious(n: &u64) -> bool {
-    is_prime(n.count_ones())
 }
 
 #[cfg(test)]
@@ -29,19 +29,24 @@ mod tests {
 
     #[test]
     fn pernicious_iter() {
-        let exp = &[3u64, 5, 6, 7, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 22, 24, 25, 26, 28,
-                    31, 33, 34, 35, 36];
-        for (act, &exp) in pernicious().take(30).zip(exp.iter()) {
+        let exp = &[
+            3, 5, 6, 7, 9, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 22, 24, 25, 26, 28, 31, 33, 34,
+            35, 36,
+        ];
+        for (act, &exp) in pernicious().zip(exp.iter()) {
             assert_eq!(act, exp);
         }
     }
 
     #[test]
     fn is_pernicious_range() {
-        let exp = &[888888877u64, 888888878, 888888880, 888888883, 888888885, 888888886];
-        for (act, &exp) in (888_888_877u64..888_888_888)
+        let exp = &[
+            888888877, 888888878, 888888880, 888888883, 888888885, 888888886
+        ];
+        for (act, &exp) in (888_888_877..888_888_888)
             .filter(is_pernicious)
-            .zip(exp.iter()) {
+            .zip(exp.iter())
+        {
             assert_eq!(act, exp);
         }
     }
